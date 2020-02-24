7936520203e55a35878e6319a95d2c64bbdb01b9
rentoslo <59967347+rentoslo@users.noreply.github.com>
2020-02-24 11:32:33 -0300

    configuração

--- src/models/users.js
+++ src/models/users.js
@@ -1,6 +1,12 @@
 const users = [
-    { id: Math.floor(Math.random()*999), name: "John", email: "john@mail.com", password: "john123" },
-    { id: Math.floor(Math.random()*999), name: "Sarah", email: "sarah@mail.com", password: "sarah123" },
-    { id: Math.floor(Math.random()*999), name: "Renato", email: "renato@", password: "123456" }
+  {
+    id: Math.floor(Math.random() * 999), name: 'John', email: 'john@mail.com', password: 'john123',
+  },
+  {
+    id: Math.floor(Math.random() * 999), name: 'Sarah', email: 'sarah@mail.com', password: 'sarah123',
+  },
+  {
+    id: Math.floor(Math.random() * 999), name: 'Renato', email: 'renato@', password: '123456',
+  },
 ];
 module.exports = users;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const controlleUser = require('./controllers/user');
 
 const routes = Router();
 
-routes.get("/listUser", controlleUser.listUser);
-routes.post("/addUser", controlleUser.addUser)
-routes.put("/updateUser", controlleUser.updateUser);
-routes.delete("/deleteUser", controlleUser.deleteUser);
+routes.get('/listUser', controlleUser.listUser);
+routes.post('/addUser', controlleUser.addUser);
+routes.put('/updateUser', controlleUser.updateUser);
+routes.delete('/deleteUser', controlleUser.deleteUser);
 
 module.exports = routes;
 
